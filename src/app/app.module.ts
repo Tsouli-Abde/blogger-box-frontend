f2d96e284d0d4db824614d90dcc26283b0200c7e
Tsouli-Abde <93455423+Tsouli-Abde@users.noreply.github.com>
2025-05-08 22:33:53 +0200

    post-list and post-list-item implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './components/top-bar/top-bar.component';
+import { provideHttpClient } from '@angular/common/http';
+import { PostService } from './services/post.service';
+import { PostListItemComponent } from './components/post-list-item/post-list-item.component';
+import { PostListComponent } from './components/post-list/post-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    PostListComponent,
+    PostListItemComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    provideHttpClient(),
+    PostService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/services/post.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable,of } from "rxjs";
+
+import { POSTS,Post } from "../data/post";
+
+@Injectable()
+export class PostService{
+    constructor(private http:HttpClient){}
+
+    getPosts():Observable<Post[]>{
+        return of(POSTS);
+    }
+}
